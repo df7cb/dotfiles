2b40185f59fd6e7ba0b51a9530f1bc27b8e7b599
Christoph Berg <cb@df7cb.de>
2000-09-18 23:32:34 +0000

    perl part of nv
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@212 fe09b2a2-98fd-0310-b664-c0abd371c07e

---
+++ bin/nv.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# $Id$
+
+if (@ARGV < 2) {
+	die "usage: $0 file... directory\n";
+}
+
+$dir = pop @ARGV;
+
+if (! -d $dir) {
+	print STDERR "nv: create $dir? ";
+	$_ = <STDIN>;
+	exit 2 unless /^y/i;
+	mkdir $dir, 0777 or die "mkdir: $!";
+}
+
+exit 1 if system "mv -iv '" .join("' '", @ARGV). "' $dir 1>&2";
+
+print "$dir\n";
