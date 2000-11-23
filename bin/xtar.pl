1bb3c991d7ef8f25ff93c5473fb0853f1c1060ce
Christoph Berg <cb@df7cb.de>
2000-11-23 21:08:04 +0000

    nix debug
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@232 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- bin/xtar.pl
+++ bin/xtar.pl
@@ -6,12 +6,12 @@ $_ = <T> || 0 or exit 1;
 ($prefix) = m|(.+)/|;
 print STDERR;
 
-print STDERR "prefix: $prefix\n";
+#print STDERR "prefix: $prefix\n";
 while(<T>) {
 	print STDERR;
 	"$prefix/###$_" =~ m|^(.*)/.*?###\1|;
 	$prefix = $1;
-	print STDERR "prefix: $prefix _: $_\n";
+	#print STDERR "prefix: $prefix _: $_\n";
 }
 
 if($prefix eq "") { exit 1 } else { print "$prefix\n"; exit 0 }
