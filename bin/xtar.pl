40980acbc823f9733bd45a942972b48c89ca1e55
Christoph Berg <cb@df7cb.de>
2002-01-01 16:41:13 +0000

    tar, tar.bz2
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@442 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- bin/xtar.pl
+++ bin/xtar.pl
@@ -1,17 +1,19 @@
 #!/usr/bin/perl
 # $Id$
 
-open T, "tar xfvz @ARGV |" or die "tar: $!";
+my $targs = "xfv";
+$targs = "xfvz" if $ARGV[0] =~ /gz$/;
+$targs = "xfvj" if $ARGV[0] =~ /bz2$/;
+
+open T, "tar $targs @ARGV |" or die "tar: $!";
 $_ = <T> || 0 or exit 1;
 ($prefix) = m|(.+)/|;
 print STDERR;
 
-#print STDERR "prefix: $prefix\n";
 while(<T>) {
 	print STDERR;
 	"$prefix/###$_" =~ m|^(.*)/.*?###\1|;
 	$prefix = $1;
-	#print STDERR "prefix: $prefix _: $_\n";
 }
 
 if($prefix eq "") { exit 1 } else { print "$prefix\n"; exit 0 }
