fe230b522017cf0b2ac9e944ae89274ea576ec68
Christoph Berg <cb@df7cb.de>
2003-01-20 13:30:38 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@629 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- lib/addressbook/addressbook.pm
+++ lib/addressbook/addressbook.pm
@@ -8,21 +8,29 @@ sub addr_get_line {
 # Christoph;Berg;DF7CB;Uni 18, 2405;D;66123;Saarbrücken;9.3.1977;0681/9657944, 0179/4530792;5573;3065;cb@heim-d.uni-sb.de, cb@cs.uni-sb.de;rw4.cs.uni-sb.de/~cb/;vdl;
 
 	my @line = split /;/, $line;
-	$vorname	= $line[0];
-	$nachname	= $line[1];
-	$zusatz		= $line[2];
-	$strasse	= $line[3];
-	$land		= $line[4];
-	$plz		= $line[5];
-	$ort		= $line[6];
-	$geburtstag	= $line[7];
-	$tel_priv	= $line[8];
-	$tel_arbeit	= $line[9];
-	$fax		= $line[10];
-	$email		= $line[11];
-	$www		= $line[12];
-	$kategorie	= $line[13];
-	$bemerkung	= $line[14];
+	$vorname	= $line[0] || "";
+	$nachname	= $line[1] || "";
+	$zusatz		= $line[2] || "";
+	$strasse	= $line[3] || "";
+	$land		= $line[4] || "";
+	$plz		= $line[5] || "";
+	$ort		= $line[6] || "";
+	$geburtstag	= $line[7] || "";
+	if($geburtstag ne "") {
+		$geburtstag =~ /^(?:(\d+)\.(\d+)\.)?(\d*)$/;
+		$geb_tag = $1;
+		$geb_mon = $2;
+		$geb_jahr = $3;
+	} else {
+		undef $geb_tag; undef $geb_mon; undef $geb_jahr;
+	}
+	$tel_priv	= $line[8] || "";
+	$tel_arbeit	= $line[9] || "";
+	$fax		= $line[10] || "";
+	$email		= $line[11] || "";
+	$www		= $line[12] || "";
+	$kategorie	= $line[13] || "";
+	$bemerkung	= $line[14] || "";
 
 	return $line;
 }
