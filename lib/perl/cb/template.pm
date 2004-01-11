ce9f89c04f7c21ab898e6fdfeb03556ccf2416a0
Christoph Berg <cb@df7cb.de>
2004-01-11 17:48:11 +0000

    template_print, more basic stop marker
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@930 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- lib/perl/cb/template.pm
+++ lib/perl/cb/template.pm
@@ -5,7 +5,6 @@ my $template_file;
 my %SUBST_STR;
 my %SUBST_SUB;
 my $template_output = 0;
-my $template_stop_rest;
 
 sub template_file {
 	$template_file = shift || die "template_file: no filename given";
@@ -27,36 +26,37 @@ sub template_sub {
 	$SUBST_SUB{$k} = $v;
 }
 
+sub template_print {
+	my $l = shift;
+	$l =~ s/__IF\((.*)\)\((.*)\)__/eval($1) ? $2 : "";/eg; # __IF(cond)(value)__
+	foreach my $k (keys %SUBST_SUB) {
+		my $f = $SUBST_SUB{$k};
+		$k =~ /^__(\w+)__$/;
+		my $k2 = "__$1\\((.*)\\)__";
+		if($l =~ /$k2/) {
+			my $return = &$f($1);
+			$l =~ s/$k2/$return/g;
+		}
+		$l =~ s/$k/&$f()/eg;
+	}
+	foreach my $k (keys %SUBST_STR) {
+		$l =~ s/$k/$SUBST_STR{$k}/g;
+	}
+	print $l;
+}
+
 sub template_output {
 	die "template_output: no output file defined / already spilled out" unless $template_file;
-	my $stop = shift; # stop marker
 	unless ($template_output) {
 		open TEMPLATE, $template_file or die "$template_file: $!";
 		$template_output = 1;
 	}
-	if($template_stop_rest) {
-		print $template_stop_rest;
-		undef $template_stop_rest;
-	}
+	my $stop = shift;
 	my $l;
 	while(defined($l = <TEMPLATE>)) {
-		$l =~ s/__IF\((.*)\)\((.*)\)__/eval($1) ? $2 : "";/eg; # __IF(cond)(value)__
-		foreach my $k (keys %SUBST_SUB) {
-			$l =~ s/$k/&{$SUBST_SUB{$k}}/eg;
-			$k =~ /^__(\w+)__$/;
-			$l =~ s/__$1\((.*)\)__/&{$SUBST_SUB{$k}}($1)/eg;
-		}
-		foreach my $k (keys %SUBST_STR) {
-			$l =~ s/$k/$SUBST_STR{$k}/g;
-		}
-		if($stop and $l =~ /(.*)$stop(.*)/s) {
-			print $1;
-			$template_stop_rest = $2;
-			last;
-		}
-		print $l;
+		last if ($stop and $l =~ /$stop/);
+		template_print($l);
 	}
-	undef $template_file unless $stop;
 }
 
 # TODO: template_output should probably not be called upon die()
