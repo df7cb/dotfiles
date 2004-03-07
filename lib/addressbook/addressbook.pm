9f75af132215024599ea0c3dc20843de49f31c5c
Christoph Berg <cb@df7cb.de>
2004-03-07 13:22:03 +0000

    OOP
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@966 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- lib/addressbook/addressbook.pm
+++ lib/addressbook/addressbook.pm
@@ -1,42 +1,45 @@
 # $Id$
 #
 
+package addressbook;
+
 sub addr_get_line {
 	my $line = shift;
 	chomp $line;
 
 # Christoph;Berg;DF7CB;Uni 18, 2405;D;66123;Saarbrücken;9.3.1977;0681/9657944, 0179/4530792;5573;3065;cb@heim-d.uni-sb.de, cb@cs.uni-sb.de;rw4.cs.uni-sb.de/~cb/;vdl;
 
-	my @line = split /;/, $line;
-	$vorname	= $line[0] || "";
-	$nachname	= $line[1] || "";
-	$zusatz		= $line[2] || "";
-	$strasse	= $line[3] || "";
-	$land		= $line[4] || "";
-	$plz		= $line[5] || "";
-	$ort		= $line[6] || "";
-	$geburtstag	= $line[7] || "";
-	if($geburtstag ne "") {
-		$geburtstag =~ /^(?:(\d+)\.(\d+)\.)?(\d*)$/;
-		$geb_tag = $1;
-		$geb_mon = $2;
-		$geb_jahr = $3;
-	} else {
-		undef $geb_tag; undef $geb_mon; undef $geb_jahr;
+	my @line = split /;/, $line, -1;
+	my $fields = {
+		vorname		=> $line[0],
+		nachname	=> $line[1],
+		zusatz		=> $line[2],
+		strasse		=> $line[3],
+		land		=> $line[4],
+		plz			=> $line[5],
+		ort			=> $line[6],
+		geburtstag	=> $line[7],
+		tel_priv	=> $line[8],
+		tel_arbeit	=> $line[9],
+		fax			=> $line[10],
+		email		=> $line[11],
+		www			=> $line[12],
+		kategorie	=> $line[13],
+		bemerkung	=> $line[14],
+	};
+
+	if($fields->{geburtstag}) {
+		$fields->{geburtstag} =~ /^(?:(\d+)\.(\d+)\.)?(\d*)$/;
+		$fields->{geb_tag} = $1;
+		$fields->{geb_mon} = $2;
+		$fields->{geb_jahr} = $3;
 	}
-	$tel_priv	= $line[8] || "";
-	$tel_arbeit	= $line[9] || "";
-	$fax		= $line[10] || "";
-	$email		= $line[11] || "";
-	$www		= $line[12] || "";
-	$kategorie	= $line[13] || "";
-	$bemerkung	= $line[14] || "";
 
-	return $line;
+	bless $fields;
 }
 
-sub addr_set_line {
-	return "$vorname;$nachname;$zusatz;$strasse;$land;$plz;$ort;$geburtstag;$tel_priv;$tel_arbeit;$fax;$email;$www;$kategorie;$bemerkung";
-}
+#sub addr_set_line {
+#	return "$vorname;$nachname;$zusatz;$strasse;$land;$plz;$ort;$geburtstag;$tel_priv;$tel_arbeit;$fax;$email;$www;$kategorie;$bemerkung";
+#}
 
 1;
