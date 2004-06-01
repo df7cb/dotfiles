77c909225aa0cd46a3cc987efb8af5ddac9aff86
Christoph Berg <cb@df7cb.de>
2004-06-01 22:42:01 +0000

    ace, deb
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@1042 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- bin/xtar.pl
+++ bin/xtar.pl
@@ -5,6 +5,7 @@ my $cmd = "";
 $cmd = "tar xfv"  if $ARGV[0] =~ /\.tar$/;
 $cmd = "tar xfvz" if $ARGV[0] =~ /\.(tgz|tar\.gz|tar\.Z)$/;
 $cmd = "tar xfvj" if $ARGV[0] =~ /\.tar\.bz2$/;
+$cmd = "unace x" if $ARGV[0] =~ /\.ace$/;
 $cmd = "ar xv" if $ARGV[0] =~ /\.(ar|deb)$/;
 $cmd = "unarj x" if $ARGV[0] =~ /\.arj$/;
 $cmd = "unrar x" if $ARGV[0] =~ /\.rar$/;
