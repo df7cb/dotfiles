e0df81fb3976730ca603cecda0200e9043ac8697
Christoph Berg <cb@df7cb.de>
2004-06-15 16:27:47 +0000

    utf-8
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/trunk/cb@1064 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- .mutt/signature.cs
+++ .mutt/signature.cs
@@ -1,5 +1,5 @@
 
 Christoph
 -- 
-Christoph Berg <cb@cs.uni-sb.de>, http://www.df7cb.de/cs/, 0681/9657944
-Universit‰t des Saarlandes, Compiler Design Lab
+cb@cs.uni-sb.de | http://www.df7cb.de/cs/
+Universit√§t des Saarlandes, Compiler Design Lab
