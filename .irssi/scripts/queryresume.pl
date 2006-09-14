856e4e6627b741e81c341b22ba77ca89fba55c8a
Christoph Berg <cb@df7cb.de>
2006-09-14 13:20:55 +0000

    check in my version
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@1675 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- .irssi/scripts/queryresume.pl
+++ .irssi/scripts/queryresume.pl
@@ -1,5 +1,12 @@
 # QueryResume by Stefan Tomanek <stefan@pico.ruhr.de>
 #
+# Modified by Joerg Jaspert <joerg@debian.org> and Christoph Berg
+#  <myon@debian.org>:
+# - Works in channels or queries now.
+# - doesnt print the box, so it restores how the window looked before you
+#   closed it, including timestamps (code stolen from buf.pl).
+# - One exception - it only restores chat text, not crap like whois.
+
 use strict;
 
 use vars qw($VERSION %IRSSI);
@@ -18,22 +25,12 @@ use Irssi 20020324;
 use Date::Format;
 use File::Glob ':glob';
 
-sub draw_box ($$$$) {
-    my ($title, $text, $footer, $colour) = @_;
-    my $box = '';
-    $box .= '%R,--[%n%9%U'.$title.'%U%9%R]%n'."\n";
-    foreach (split(/\n/, $text)) {
-        $box .= '%R|%n '.$_."\n";
-    }
-    $box .= '%R`--<%n'.$footer.'%R>->%n';
-    $box =~ s/%.//g unless $colour;
-    return $box;
-}
+my $timestamp_re = '^--- (?:Log opened|Day changed) ([^:]+) ';
 
 sub sig_window_item_new ($$) {
     my ($win, $witem) = @_;
-    #return unless (ref $witem && $witem->{type} eq 'QUERY');
-    return unless (ref $witem);
+    return unless (ref $witem && $witem->{type} eq 'QUERY');
+    #return unless (ref $witem);
     my @data;
     my $filename = Irssi::settings_get_str('autolog_path');
     my $servertag = $witem->{server}->{tag};
@@ -45,21 +42,54 @@ sub sig_window_item_new ($$) {
     $filename = strftime($filename, @lt, $zone);
     $filename =~ s/(\[|\])/\\$1/g;
     local *F;
-    open(F, "<".bsd_glob($filename));
+    open(F, "<".bsd_glob($filename)) or return;
     my $lines = Irssi::settings_get_int('queryresume_lines');
+    my $timestamp_line;
+    my $timestamp_day = "";
+
+    #--- Log opened Do Jul 06 00:00:01 2006
+    #--- Day changed Do Jul 06 2006
+    #02:03 -!- Irssi: Starting query in oftc with Ganneff
+    #--- Log closed Fr Jul 07 00:00:36 2006
+
     foreach (<F>) {
-	unless (/^--- Log/) {
-	    push(@data, $_);
-	    shift(@data) if (@data > $lines);
+	next if (/^--- Log closed/ || /^[\d:]+ [(-]![-)] /);
+	if (/$timestamp_re/o) {
+	    if ($timestamp_day eq $1) {
+		next;
+	    } else {
+		pop(@data) if ($timestamp_line); # remove repeated timestamp lines
+		$timestamp_line = 1;
+		$timestamp_day = $1;
+		s/ \d\d:\d\d:\d\d//;
+	    }
+	} else {
+	    $timestamp_line = 0;
+	}
+	push(@data, $_);
+	if (@data > $lines) {
+	    if ($data[0] =~ /$timestamp_re/o) {
+		splice @data, 1, 1; # keep the first timestamp line
+		if ($data[1] =~ /$timestamp_re/o) { # nothing interesting left for this day
+		    shift @data;
+		}
+	    } else {
+		shift @data;
+	    }
 	}
     }
-    my $text;
-    $text .= $_ foreach @data;
-    $text =~ s/%/%%/g;
-    $witem->print(draw_box('QueryResume', $text, $filename, 1), MSGLEVEL_CLIENTCRAP) if $text;
+    pop @data if ($data[-1] =~ /$timestamp_re/o);
+    my $text = join '', @data;
+
+    my $view = $win->view;
+    $view->remove_all_lines();
+    $view->redraw();
+    $win->gui_printtext_after(undef, MSGLEVEL_NEVER, "--- Reading log $filename\n$text");
+    $view->redraw();
 }
 
-Irssi::settings_add_int($IRSSI{name}, 'queryresume_lines', 10);
+Irssi::settings_add_int($IRSSI{name}, 'queryresume_lines', 15);
 
 Irssi::signal_add('window item new', 'sig_window_item_new');
 
+# vim:sw=4:
