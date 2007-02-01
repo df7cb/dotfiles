b43f0accb45abe3cfcef8d29778574a4a7a5fb94
Christoph Berg <cb@df7cb.de>
2007-02-01 12:13:55 +0000

    akilluser.pl: also kill user@host, split reason and "mail support" trailer, add some error catching
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@1731 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- .irssi/scripts/akilluser.pl
+++ .irssi/scripts/akilluser.pl
@@ -2,6 +2,7 @@
 # in the command
 #
 # (C) 2006 by Joerg Jaspert <joerg@debian.org>
+# (C) 2007 by Christoph Berg <cb@df7cb.de>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -28,7 +29,7 @@ $VERSION = '0.0.0.0.1.alpha.0.0.1';
 %IRSSI = (
     authors     => 'Joerg Jaspert',
     contact     => 'joerg@debian.org',
-    name  => 'akilluser',
+    name        => 'akilluser',
     description => 'AKILLS a nick',
     license     => 'GPL v2 (and no later)',
 );
@@ -40,25 +41,44 @@ sub akill_nick {
   my ($arg, $server, $channel) = @_;
 
   $arg =~ /(\S+)\s?(.*)?/;
-  my $nick = $1;
-  my $reason = $2;
+  my ($target, $reason) = ($1, $2);
+  my ($user, $host);
+
+  if ($target =~ /(.+)@(.+)/) {
+    ($user, $host) = ($1, $2);
+  } else {
+    if (!$channel) {
+      Irssi::print("Not joined to a channel");
+      return;
+    }
+    my $nickh = $channel->nick_find($target);
+    if (!$nickh->{host}) {
+      Irssi::print("User $target not found on $channel->{name}");
+      return;
+    }
+    if ($nickh->{host} =~ /\.oftc\.net$/) {         # Do not AKILL staff from oftc.
+      Irssi::print("Not AKILLing OFTC staff");
+      return;
+    }
+    $nickh->{host} =~ /(\S+)@(\S+)/;
+    ($user, $host) = ("*", $2);
+  }
+
+  if ("$user$host" !~ /[\w\d]/) {
+    Irssi::print("AKILLing $user\@$host looks insane");
+    return;
+  }
+
   if (length($reason) < 2) {
-	$reason = Irssi::settings_get_str('akill_reason');
+    $reason = Irssi::settings_get_str('akill_reason');
   }
-  my $channame = $channel->{name};
-
-  my $nickh=$channel->nick_find($nick);
-  if ($nickh->{host} =~ /.*oftc.net/) {         # Do not AKILL staff from oftc.
-	Irssi::print("Not AKILLing OFTC staff");
-	return;
+  if ($reason !~ /\@oftc\.net/) {
+    $reason .= " " . Irssi::settings_get_str('akill_trailer');
   }
 
-  $nickh->{host} =~ /(\S+)@(\S+)/;
-  my $user = $1;
-  my $khost = $2;
   my $window = Irssi::active_win();
-  $window->print("AKILLed $nick (ident $user) at $khost with \"$reason\"");
-  $server->command("quote os akill add *\@$khost $reason");
+  $window->print("AKILLed $target ($user\@$host) with \"$reason\"");
+  $server->command("quote os akill add $user\@$host $reason");
 }
 
 
@@ -66,6 +86,7 @@ sub akill_nick {
 # ---------- Do the startup tasks ----------
 
 # Add the settings
-Irssi::settings_add_str("akilluser.pl", "akill_reason", 'This host violated network policy. If you feel an error has been made, please contact support@oftc.net, thanks.');
+Irssi::settings_add_str("akilluser.pl", "akill_reason", 'This host violated network policy.');
+Irssi::settings_add_str("akilluser.pl", "akill_trailer", 'Mail support@oftc.net if you think this in error.');
 
 Irssi::command_bind('akill', 'akill_nick');
