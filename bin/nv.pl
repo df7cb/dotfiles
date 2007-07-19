7fd1dc92bfe599542b2d7944098268e70e9c76cd
Christoph Berg <cb@df7cb.de>
2007-07-19 08:09:54 +0000

    support nv a a
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@1857 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- bin/nv.pl
+++ bin/nv.pl
@@ -1,19 +1,37 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # $Id$
+
+use strict;
 
 if (@ARGV < 2) {
 	die "usage: $0 file... directory\n";
 }
 
-$dir = pop @ARGV;
+my $dir = pop @ARGV;
+my $dir2;
 
 if (! -d $dir) {
+	if (-e $dir) {
+		if (not grep { $dir = $_ } @ARGV) {
+			print STDERR "$dir exists, but is not a file that would be moved\n";
+			exit 2;
+		}
+		$dir2 = $dir.$$;
+	}
+
 	print STDERR "nv: create $dir? ";
 	$_ = <STDIN>;
-	exit 2 unless /^y/i;
-	mkdir $dir, 0777 or die "mkdir: $!";
+	exit 2 unless /^[yjo]/i;
+
+	mkdir $dir2 || $dir, 0777 or die "mkdir: $!";
 }
 
-exit 1 if system "mv -iv '" .join("' '", map { s/'/'\\''/g; $_; } @ARGV). "' '$dir' 1>&2";
+exit 1 if system "mv -iv '" .
+	join("' '", map { s/'/'\\''/g; $_; } @ARGV) .
+	"' '". ($dir2 || $dir) ."' 1>&2";
+
+if ($dir2) {
+	rename $dir2, $dir or die "$dir: $!";
+}
 
 print "$dir\n";
