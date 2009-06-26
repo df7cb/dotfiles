ef0a784c71768de9a0a0f491f0f5bde847df52f1
Christoph Berg <cb@df7cb.de>
2009-06-26 09:39:12 +0000

    munge last arg
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2165 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- bin/mbsync.sh
+++ bin/mbsync.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-CHANNEL="$1"
-HOST=$(echo "$CHANNEL" | sed -e 's/[.\/ -].*//')
+HOST=$(eval echo \${$#} | sed -e 's/[.\/ -].*//')
 LOCK=$HOME/mail.mbsync.$HOST.lock
 
 if ip addr show dev eth0 2> /dev/null | fgrep -q 192.168.17. ; then
 	SOCKS=tsocks
 fi
 
-$SOCKS flock -n "$LOCK" mbsync -q "$CHANNEL"
+$SOCKS flock -n "$LOCK" mbsync "$@"
