ad436b643804b253131addbafce2833eb96d55a8
Christoph Berg <cb@df7cb.de>
2009-08-08 18:38:59 +0000

    fix lock file name
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2174 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- bin/mbsync.sh
+++ bin/mbsync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 HOST=$(eval echo \${$#} | sed -e 's/[.\/ -].*//')
-LOCK=$HOME/mail.mbsync.$HOST.lock
+LOCK=$HOME/mail/.mbsync.$HOST.lock
 
 if ip addr show dev eth0 2> /dev/null | fgrep -q 192.168.17. ; then
 	SOCKS=tsocks
