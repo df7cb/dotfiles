64d4d88472a414d8ed929873d6515a1bdf6412f9
Christoph Berg <cb@df7cb.de>
2010-12-30 19:40:53 +0000

    dwdiff
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2284 fe09b2a2-98fd-0310-b664-c0abd371c07e

---
+++ bin/wdiff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while getopts "uL:" opt ; do
+	case $opt in
+	u) ;;
+	L) ;; # svn: -L --label
+	*) exit 5 ;;
+	esac
+done
+# shift away args
+shift $(($OPTIND - 1))
+
+if [ -x /usr/bin/dwdiff ] ; then
+	/usr/bin/dwdiff -w '[31;47m' -x '[0m' -y '[32;47m' -z '[0m' \
+		-p -d '#-(){}[]' \
+		-C 3 "$@"
+else
+	/usr/bin/wdiff -w '[31;47m' -x '[0m' -y '[32;47m' -z '[0m' \
+		"$@"
+fi
