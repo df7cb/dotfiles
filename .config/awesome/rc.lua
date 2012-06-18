42379f2c253f7c13c30c04558bfd7923fa381d75
Christoph Berg <cb@df7cb.de>
2012-06-18 14:07:12 +0000

    import theme
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2406 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -37,7 +37,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
