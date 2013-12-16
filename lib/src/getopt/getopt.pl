9c451c9cecf1dca4f13138b7d12a9b26b5bd8d73
Christoph Berg <cb@df7cb.de>
2013-12-16 16:29:39 +0000

    getopt::long
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2485 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- lib/src/getopt/getopt.pl
+++ lib/src/getopt/getopt.pl
@@ -6,3 +6,18 @@ getopts('abc:');
 print "\$opt_a = $opt_a\n";
 print "\$opt_b = $opt_b\n";
 print "\$opt_c = $opt_c\n";
+
+
+use Getopt::Long;
+Getopt::Long::config('bundling');
+if (!GetOptions (
+		'-h'              =>  \$params->{'help'},
+		'--help'          =>  \$params->{'help'},
+		'--version'       =>  \$params->{'version'},
+		'--local-user=s'  =>  \$params->{'local-user'},
+		'-m:s'              =>  \$params->{'mail'},
+		'--mail:s'          =>  \$params->{'mail'},
+		'--key-file=s@'   =>  \$params->{'key-files'},
+	)) {
+	usage(\*STDERR, 1);
+};
