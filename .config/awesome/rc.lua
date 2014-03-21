7a6c2f28876993c42c9fa0b98f5f575223b9c881
Christoph Berg <cb@df7cb.de>
2014-03-21 19:54:20 +0000

    M-b
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2500 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -211,7 +211,10 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
-
+    awful.key({ modkey,           }, "b",
+        function ()
+            mywibox[mouse.screen].visible = not mywibox[mouse.screen].visible
+        end),
     awful.key({ modkey,           }, "j",
         function ()
             awful.client.focus.byidx( 1)
