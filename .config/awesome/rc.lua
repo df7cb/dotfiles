4d10ee73bae79bf644e53be167be7f9975deaae1
Christoph Berg <cb@df7cb.de>
2015-05-17 19:24:23 +0000

    shlvl
    git-svn-id: svn+ssh://svn.df7cb.de/svn/dotfiles/cb@2580 fe09b2a2-98fd-0310-b664-c0abd371c07e

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -40,7 +40,7 @@ end
 beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "env SHLVL=0 x-terminal-emulator"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
