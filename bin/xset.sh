bb40202982f41e2f7552dad14e03eae6bce752cc
Christoph Berg <myon@debian.org>
2021-10-31 13:08:36 +0100

    Move xserver setup to bin/xset.sh

---
+++ bin/xset.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+xhost - >/dev/null
+xmodmap $HOME/.xmodmap-pc
+xset b off
+xset r rate 400 30
+xset dpms 0 0 1200
+if command -v synclient >/dev/null; then
+  synclient TapButton1=1 SingleTapTimeout=50 #HorizTwoFingerScroll=1
+fi
+
+xrdb $HOME/.Xdefaults
+if [ -f $HOME/.Xdefaults-local ]; then
+  xrdb -merge $HOME/.Xdefaults-local
+fi
