d9ddb6edc3587033664e566115be42d05b9e6de6
Christoph Berg <myon@debian.org>
2022-07-20 12:29:03 +0200

    awesome: Ctrl-ThinkVantage -> hibernate

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -391,6 +391,7 @@ globalkeys = gears.table.join(
     awful.key({ }, "KP_Multiply", function () awful.util.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle") end),
     awful.key({ }, "Pause", function () awful.util.spawn("spotify_control playpause") end),
     awful.key({     }, "XF86Launch1", function () awful.util.spawn("xscreensaver-command -lock") end), -- ThinkVantage button on L520
+    awful.key({ "Control" }, "XF86Launch1", function () awful.util.spawn("xscreensaver-command -lock; sleep 1; sudo systemctl hibernate") end), -- ThinkVantage button on L520
     awful.key({ "Control" }, "Pause", function () awful.util.spawn("xscreensaver-command -lock") end)
 )
 
