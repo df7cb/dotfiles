cd96ddc395f2d793091c7f291d0db0d8e9514a4e
Christoph Berg <myon@debian.org>
2023-08-08 16:06:03 +0200

    Add debcheckout.sh

---
+++ bin/debcheckout.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu
+
+SRC=$1
+
+# create 1st directory level
+mkdir -p $SRC
+cd $SRC
+
+# checkout Debian repository into 2nd directory level
+debcheckout $SRC
+cd $SRC
+v up
+git ctags
+
+case $(git remote -v) in
+    *github.com/*) # Debian repo is on github, don't try to checkout upstream
+        exit
+        ;;
+esac
+cd ..
+
+# checkout upstream repository into 2nd directory level
+HOMEPAGE=$(apt-cache showsrc $SRC | awk '/^Homepage:/ { print $2 }')
+case $HOMEPAGE in
+    *github.com/*)
+        git clone ${HOMEPAGE%/}.git $SRC.git
+        cd $SRC.git
+        git ctags
+        ;;
+esac
